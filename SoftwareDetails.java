ac4e3eb368bc2357d350617b4cb63d6f22d22a66
mounikajayam <119663957+mounikajayam@users.noreply.github.com>
2022-12-02 18:19:06 +0530

    Add files via upload

---
+++ SoftwareDetails.java
@@ -0,0 +1,60 @@
+package com.example.com.example.software;
+
+public class SoftwareDetails {
+	private String softwarename;
+	private int software_id;
+	private String softwaredescription;
+	private int price;
+	private String companyname;
+	private int rating;
+	private int stock;
+	public String getsoftwarename() {
+		return softwarename;
+	}
+	public void setsoftwarename(String softwarename) {
+		this.softwarename = softwarename;
+	}
+	public int getsoftware_id() {
+		return software_id;
+	}
+	public void setsoftware_id(int software_id) {
+		this.software_id = software_id;
+	}
+	public String getsoftwaredescription() {
+		return softwaredescription;
+	}
+	public void setsoftwaredescription(String softwaredescription) {
+		this.softwaredescription = softwaredescription;
+	}
+	public int getprice() {
+		return price;
+	}
+	public void setprice(int price) {
+		this.price = price;
+	}
+	public String getcompanyname() {
+		return companyname;
+	}
+	public void setcompanyname(String companyname) {
+		this.companyname = companyname;
+	}
+	public int getrating() {
+		return rating;
+	}
+	public void setrating(int rating) {
+		this.rating = rating;
+	}
+	public int getstock() {
+		return stock;
+	}
+	public void setstock(int stock) {
+		this.stock = stock;
+	}
+
+	}
+	
+	
+	
+	
+
+
